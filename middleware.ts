c4759930f2af830ca2edf4aadc7b720c62cd0f79
Victor Seleimend <sseleimend@gmail.com>
2025-07-05 10:43:17 -0300

    Implement authentication middleware with role-based redirection and public route handling

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,49 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { MiddlewareConfig } from "next/server";
+import {
+  clerkClient,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook/register",
+  "/sign-up",
+  "/sign-in",
+]);
+
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  try {
+    const { userId } = await auth();
+
+    if (!userId && isPublicRoute(req)) return;
+
+    if (!userId && !isPublicRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+
+    const client = await clerkClient();
+    const user = await client.users.getUser(userId);
+    const role = user.publicMetadata.role as string | undefined;
+
+    if (role === "admin" && req.nextUrl.pathname === "/dashboard") {
+      return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+    }
+
+    if (role !== "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+
+    if (isPublicRoute(req)) {
+      return NextResponse.redirect(
+        new URL(role === "admin" ? "/admin/dashboard" : "/dashboard", req.url)
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.redirect(new URL("/error", req.url));
+  }
+});
 
 export const config: MiddlewareConfig = {
   matcher: [
